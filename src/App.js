75a7aba8a7e9b6724f414bd27a1f385990c84c5a
Lasanga Dissanayaka <129869155+lasanga890@users.noreply.github.com>
2024-11-19 09:15:00 +0000

    get data to parent class from child class

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
-
-import './App.css';
-import {students} from '../src/assets/data/studentdb.js'
-import StudentTable from './components/StudentTable';
+import "./App.css";
+import { students } from "../src/assets/data/studentdb.js";
+import StudentTable from "./components/StudentTable";
 
 function App() {
   return (
     <>
-    <h1>Students Information Portal</h1>
-
-    <table>
-      <tbody>
-      <tr>
-        <td><StudentTable students={students} /></td>
-
-      </tr>
-      </tbody>
-    </table>
+      <div className="App">
+        <h1>Students Information Portal</h1>
+        <hr />
+        <table>
+          <tbody>
+            <tr>
+              <td>
+                <StudentTable students={students} />
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </>
   );
 }
--- src/components/StudentTable.js
+++ src/components/StudentTable.js
@@ -1,22 +1,37 @@
-export default function StudentTable(props){
-    return(
-        <>
-        <div className="outerDiv">
-            <div className="leftDiv">
-                <table>
-                    <thead>
-                        <tr>
-                            <td>First Name</td>
-                            <td>Last Name</td>
-                            <td>Course</td>
-                            <td>Country</td>
-                            <td>Profile</td>
-                            
-                        </tr>
-                    </thead>
-                </table>
-            </div>
+import "../assets/css/compo.css";
+
+export default function StudentTable(props) {
+  return (
+    <>
+      <div className="outerDiv">
+        <div className="leftDiv">
+          <table>
+            <thead>
+              <tr>
+                <td>First Name</td>
+                <td>Last Name</td>
+                <td>Course</td>
+                <td>Country</td>
+                <td>Profile</td>
+              </tr>
+            </thead>
+            <tbody>
+              {props.students.map((student) => (
+                <tr>
+                  <td>{student.firstName}</td>
+                  <td>{student.lastName}</td>
+                  <td>{student.course}</td>
+                  <td>{student.address.country}</td>
+                <td><button>view</button></td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+          <div className="rightDiv">
+            
+          </div>
         </div>
-        </>
-    )
+      </div>
+    </>
+  );
 }
